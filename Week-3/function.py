cfc2bbb4a44b91646bf712b81467fdb8297c2c79
£hri <poojatkadhane@gmail.com>
2024-11-11 04:41:11 +0000

    first commit

---
+++ Week-3/function.py
@@ -0,0 +1,12 @@
+# Function - We can reuse the code multiple times
+# design og block to rewrite the code n no. of times
+# def means define
+# parameter is written in ()
+# def add(a,b):
+#     result = a+b
+#     return result (return is variable)
+# values of a and b will be arguements
+
+def add(a,b)
+    sum=add(5,2)
+    print(sum)
